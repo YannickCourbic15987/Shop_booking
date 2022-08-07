fc6e1db8217b88d92782d98602e3cb2fc7e60dc3
YannickCourbic15987 <a.yacine2020@laposte.net>
2022-08-07 19:56:12 +0200

    connexion réussie

--- Views/Sign/index.php
+++ Views/Sign/index.php
@@ -1,4 +1,9 @@
-<h1 class="text-center text-uppercase text-primary mt-5 ">Connexion</h1>
-<div class="container">
-    <form action="#" method="post" class="form-group"> <?= $form ?> </form>
-</div>
+<?php if (!isset($_SESSION['user'])) : ?>
+    <h1 class="text-center text-uppercase text-primary mt-5 ">Connexion</h1>
+    <div class="container">
+        <form action="#" method="post" class="form-group"> <?= $form ?> </form>
+    </div>
+<?php else : ?>
+    <h1 class="text-center text-uppercase text-primary mt-5"> mon profil : </h1>
+
+<?php endif; ?>
